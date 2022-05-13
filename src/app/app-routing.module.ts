975fc849296386c47a0b6efaef45826a1775080a
Yoan Estrada <yoanestrada01@gmail.com>
2022-05-13 15:06:16 -0500

    frist commit modules added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//Components
+
+const routes: Routes = [
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  googleMapKey: 'KEY QUE SE USA EN prod',
+};
